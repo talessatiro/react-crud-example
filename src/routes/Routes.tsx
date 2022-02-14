4ca2b90ead5e7902f6fd9f399a93ac2adf9dfd19
Tales Satiro <talessatiro@gmail.com>
2022-02-14 17:24:22 -0300

    feat: inserted react.lazy behavior in routes definition

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,21 +1,49 @@
+import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { Home, UserManagement } from '../pages';
-import {
-    UserList,
-    UserCreate,
-    UserEdit,
-} from '../pages/users-management/components';
+
+const LazyHome = React.lazy(() =>
+    import('../pages/home/Home').then((module) => ({ default: module.Home })),
+);
+
+const LazyUserManagement = React.lazy(() =>
+    import('../pages/users-management/UserManagement').then((module) => ({
+        default: module.UserManagement,
+    })),
+);
+
+const LazyUserList = React.lazy(() =>
+    import('../pages/users-management/components/user-list/UserList').then(
+        (module) => ({ default: module.UserList }),
+    ),
+);
+
+const LazyUserCreate = React.lazy(() =>
+    import('../pages/users-management/components/user-create/UserCreate').then(
+        (module) => ({ default: module.UserCreate }),
+    ),
+);
+
+const LazyUserEdit = React.lazy(() =>
+    import('../pages/users-management/components/user-edit/UserEdit').then(
+        (module) => ({ default: module.UserEdit }),
+    ),
+);
 
 export const RoutesApp = () => {
     return (
-        <Routes>
-            <Route path="/home" element={<Home />} />
-            <Route path="/users" element={<UserManagement />}>
-                <Route path="" element={<UserList />} />
-                <Route path="create" element={<UserCreate />} />
-                <Route path="edit/:id" element={<UserEdit />} />
-            </Route>
-            <Route path="*" element={<Navigate to="home" replace={true} />} />
-        </Routes>
+        <React.Suspense fallback="Loading...">
+            <Routes>
+                <Route path="/home" element={<LazyHome />} />
+                <Route path="/users" element={<LazyUserManagement />}>
+                    <Route path="" element={<LazyUserList />} />
+                    <Route path="create" element={<LazyUserCreate />} />
+                    <Route path="edit/:id" element={<LazyUserEdit />} />
+                </Route>
+                <Route
+                    path="*"
+                    element={<Navigate to="home" replace={true} />}
+                />
+            </Routes>
+        </React.Suspense>
     );
 };
